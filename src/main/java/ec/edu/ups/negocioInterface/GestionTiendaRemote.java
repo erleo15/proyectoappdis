84edc024fbb6f6dd8c6a2e3171357de9956c9ffb
erleo15 <erleo15@192.168.100.11>
2020-01-23 02:06:01 -0500

    Reestructuracion del proyecto Gestion TiendaLocal

--- src/main/java/ec/edu/ups/negocioInterface/GestionTiendaRemote.java
+++ src/main/java/ec/edu/ups/negocioInterface/GestionTiendaRemote.java
@@ -5,11 +5,28 @@ import java.util.List;
 import javax.ejb.Remote;
 
 import ec.edu.ups.modelo.Pelicula;
+import ec.edu.ups.modelo.Usuario;
 
 @Remote
 public interface GestionTiendaRemote {
 
-		//crudproductos
+		
+	/*
+	 * Parte Para Crud Usuario
+	 */
+	public boolean crearUsuario(Usuario usuario);
+	
+	
+	
+	/*
+	 * Parte Para Crud Peliculas
+	 */
+	
+	/*
+	 * Parte para crud Categoria
+	 */
+	
+	//crudproductos
 		//crudusuarios
 		//
 		public List<Pelicula> peliculasMasVendidas();
